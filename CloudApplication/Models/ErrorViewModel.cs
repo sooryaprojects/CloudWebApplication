76e5b9271dfe26750d2d63c630ef14f880017fae
soorya <sooryaprojects@gmail.com>
2021-04-27 10:11:32 -0700

    Initial Commit
    Cloud Based Application

---
+++ CloudApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CloudApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
